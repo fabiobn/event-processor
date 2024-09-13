461022ed85969e844498472f5cd3b9679eb662eb
fabiobn <fbnunes@hotmail.com>
2024-09-13 14:45:49 -0300

    Ajustes.

--- simulation/producer-event-error.sh
+++ simulation/producer-event-error.sh
@@ -4,5 +4,5 @@ echo "===================simulando envio mensagem com erro produtor 1===========
 awslocal sqs send-message \
   --queue-url http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/event-processor.fifo \
   --message-group-id 'app' \
-  --message-deduplication-id '123' \
-  --message-body '{"id": 123, "nom": "produto 1", "operacao": "COMPRA", "quantidade": 10, "origem": "app", "destino": "loja A"}'
+  --message-deduplication-id '111' \
+  --message-body '{"id": 111, "nom": "produto 1", "operacao": "COMPRA", "quantidade": 10, "origem": "app", "destino": "loja A"}'
